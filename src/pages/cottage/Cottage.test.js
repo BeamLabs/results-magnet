844e136137d6b038eeb3d45152231696fc1b6e15
Mousecop <joaskew13@gmail.com>
2017-06-28 17:10:27 -0400

    Removes dead files / folders

--- src/pages/cottage/Cottage.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Cottage from './Cottage';
-
-it('renders cottage component without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Cottage />, div);
-});
