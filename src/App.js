326a8b7acf975e19937351013b80c34ee68fb277
Mousecop <joaskew13@gmail.com>
2017-06-30 14:48:25 -0400

    Adds new component and Route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './common/header/Header';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
+import Offerings from './pages/offerings/Offerings';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
             <Header/>
             <Route exact path='/' component={Home}/>
             <Route exact path='/about' component={About}/>
+            <Route exact path='/offerings' component={Offerings}/>
           </div>
         </Router>
       </div>
